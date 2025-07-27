591e4633c09bd47019e685b037749521b14f1d23
Ian-Lusule <125305803+Ian-Lusule@users.noreply.github.com>
2025-07-27 17:12:16 +0300

    Add integrations/__init__.py

---
+++ integrations/__init__.py
@@ -0,0 +1,7 @@
+# integrations/__init__.py
+
+# Import all integration modules (they are currently empty, but this sets up the structure)
+from .vulners_api import VulnersAPI
+from .exploitdb import ExploitDB
+from .wappalyzer import WappalyzerIntegration
+from .shodan import ShodanIntegration
