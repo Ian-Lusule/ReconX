13a02c2fdf4deada0607c6fd5b2d04d269123026
Ian-Lusule <125305803+Ian-Lusule@users.noreply.github.com>
2025-07-27 17:12:47 +0300

    Add plugins/__init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,7 @@
+# plugins/__init__.py
+
+# Import all plugin modules (they are currently empty, but this sets up the structure)
+from .wordpress import WordPressPlugin
+from .joomla import JoomlaPlugin
+from .laravel import LaravelPlugin
+from .flask import FlaskPlugin
